1f8d0edbdb7d1a05e414f1ea077af44811edba73
Vasilii Bulkovskii <vasilii.bulkovskii@gmail.com>
2023-10-12 14:28:22 +0500

    add sima api

--- app/services/sima_api.rb
+++ app/services/sima_api.rb
@@ -1,27 +1,43 @@
 class SimaApi
   BASE_URL = 'https://www.sima-land.ru/api/v5'
-  API_TOKEN = ENV['API_KEY']
+  # api_key = ENV['API_KEY']
 
-  def get_categorys(p, api_token)
-    response = Faraday.get(BASE_URL + '/category', {'p': p}, {'accept': 'application/json', 'X-Api-Key': API_TOKEN})
+  def initialize(api_key)
+    @api_key = api_key 
+  end
+
+  # p - номер страница, по умолчанию 1
+  def get_categorys(p)
+    response = Faraday.get(BASE_URL + '/category', 
+    {'p': p}, 
+    {'accept': 'application/json', 'X-Api-Key': @api_key})
     
     parsed_reps = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_category(id, api_token)
-    response = Faraday.get(BASE_URL + '/category', {'id': id}, {'accept': 'application/json', 'X-Api-Key': API_TOKEN})
+  def get_category(id)
+    response = Faraday.get(BASE_URL + '/category/' + id.to_s, 
+    {}, 
+    {'accept': 'application/json', 'X-Api-Key': @api_key})
     
     parsed_reps = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_items(p, api_token)
-    response = Faraday.get(BASE_URL + '/item', {'p': p}, {'accept': 'application/json', 'X-Api-Key': API_TOKEN})
+  # p - номер страница, по умолчанию 1
+  def get_items(p)
+    response = Faraday.get(BASE_URL + '/item', 
+    {'p': p}, 
+    {'accept': 'application/json', 'X-Api-Key': @api_key})
     
     parsed_reps = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_item(id, by_sid: true, api_token: nil)
-    response = Faraday.get(BASE_URL + '/item', {'id': id, by_sid: by_sid}, {'accept': 'application/json', 'X-Api-Key': API_TOKEN})
+  # by_sid - true поиск включен по артикулу
+  # view - краткое описание товара если view = brief 
+  def get_item(id, by_sid: true, view: '')
+    response = Faraday.get(BASE_URL + '/item/' + id.to_s, 
+    {by_sid: by_sid, view: view}, 
+    {'accept': 'application/json', 'X-Api-Key': @api_key})
     
     parsed_reps = JSON.parse(response.body, symbolize_names: true)
   end
